73497ea9e5550dc730b74e1d9e2b085fc8f780cf
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-10 12:23:10 +0200

    propuesta carrito

---
+++ carrito.js
@@ -0,0 +1,12 @@
+// un array de productos
+// cada producto debe tener un tipo de iva (normal, reducido o exento)
+// { nombre: 'libreta', precio: 1.80, iva: normal }
+
+// carrito es un array (subconjunto de los productos escogidos)
+// criterio => precio <10
+
+// calcular subtotal sin iva
+
+// un array de tipos de iva { nombre:'normal', tipo: 21 }
+
+// calcular el total con iva
