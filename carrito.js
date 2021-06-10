176a8be8df045b5d44aabe5154f13a830ee6abb4
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-10 12:53:53 +0200

    solución básica

--- carrito.js
+++ carrito.js
@@ -2,10 +2,26 @@
 // cada producto debe tener un tipo de iva (normal, reducido o exento)
 // { nombre: 'libreta', precio: 1.80, iva: normal }
 
+let productos = [
+  { nombre: "libreta", precio: 1.8, iva: "normal" },
+  { nombre: "lapiz", precio: 0.5, iva: "normal" },
+  { nombre: "libro", precio: 19.9, iva: "reducido" },
+];
+
 // carrito es un array (subconjunto de los productos escogidos)
 // criterio => precio <10
 
+let carrito = [];
+productos.forEach(function (producto) {
+  if (producto.precio < 10) {
+    carrito.push(producto);
+  }
+});
+
 // calcular subtotal sin iva
+
+let subtotal = 0;
+carrito.forEach((producto) => (subtotal += producto.precio));
 
 // un array de tipos de iva { nombre:'normal', tipo: 21 }
 
