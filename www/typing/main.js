92d86ad8a2ed56a47967e508bbd5193bb1c366a0
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-16 12:07:23 +0200

    js query read

---
+++ www/typing/main.js
@@ -0,0 +1,10 @@
+console.log("🌜 aterrizando");
+
+// Ejemplo de consulta y lectura simple
+// const nodoHeader = document.querySelector("header");
+// console.log(nodoHeader);
+// console.log(nodoHeader.innerHTML);
+// const nodoHeaderH1 = document.querySelector("header h1");
+// console.log(nodoHeaderH1);
+// console.log(nodoHeaderH1.innerText);
+// nodoHeaderH1.innerText = "Otra cosa";
